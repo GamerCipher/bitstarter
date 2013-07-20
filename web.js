701e1e65daf4bed304ecde0dd83b0b3dcc260d99
GamerCipher <juankoenig@gmail.com>
2013-07-19 19:57:01 -0700

    Added contact and about pages.

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var fs=require('fs');
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  fs.readFile('index.html', function(err, data) {
+var serveFile= function(response,fname) {
+  fs.readFile(fname, function(err, data) {
     if (err) throw err;
     response.send(data.toString());
   });
+};
+
+app.get('/', function(request, response) {
+  serveFile(response,'index.html');  
+});
+
+app.get('/about', function(request, response) {
+  serveFile(response,'about.html');  
+});
+
+app.get('/contact', function(request, response) {
+  serveFile(response,'contact.html');  
 });
 
 
